46c29061a25ad681be4796122cb8ceda45084ae4
Bence Firtko <fib1mc@bosch.com>
2024-08-14 15:06:32 +0200

    first commit

---
+++ Langton's ant/Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace Langton_s_ant
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.mbStart = new System.Windows.Forms.ToolStripMenuItem();
+            this.mbStop = new System.Windows.Forms.ToolStripMenuItem();
+            this.Counter = new System.Windows.Forms.Label();
+            this.mbNewGame = new System.Windows.Forms.ToolStripMenuItem();
+            this.mbNext = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mbStart,
+            this.mbStop,
+            this.mbNewGame,
+            this.mbNext});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1082, 30);
+            this.menuStrip1.TabIndex = 2;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // mbStart
+            // 
+            this.mbStart.Name = "mbStart";
+            this.mbStart.Size = new System.Drawing.Size(54, 26);
+            this.mbStart.Text = "Start";
+            this.mbStart.Click += new System.EventHandler(this.mbStart_Click);
+            // 
+            // mbStop
+            // 
+            this.mbStop.Name = "mbStop";
+            this.mbStop.Size = new System.Drawing.Size(54, 26);
+            this.mbStop.Text = "Stop";
+            this.mbStop.Click += new System.EventHandler(this.mbStop_Click);
+            // 
+            // Counter
+            // 
+            this.Counter.AutoSize = true;
+            this.Counter.Location = new System.Drawing.Point(12, 28);
+            this.Counter.Name = "Counter";
+            this.Counter.Size = new System.Drawing.Size(60, 17);
+            this.Counter.TabIndex = 3;
+            this.Counter.Text = "Steps: 0";
+            // 
+            // mbNewGame
+            // 
+            this.mbNewGame.Name = "mbNewGame";
+            this.mbNewGame.Size = new System.Drawing.Size(96, 26);
+            this.mbNewGame.Text = "New Game";
+            this.mbNewGame.Click += new System.EventHandler(this.mbNewGame_Click);
+            // 
+            // mbNext
+            // 
+            this.mbNext.Name = "mbNext";
+            this.mbNext.Size = new System.Drawing.Size(54, 26);
+            this.mbNext.Text = "Next";
+            this.mbNext.Click += new System.EventHandler(this.mbNext_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1082, 981);
+            this.Controls.Add(this.Counter);
+            this.Controls.Add(this.menuStrip1);
+            this.DoubleBuffered = true;
+            this.MainMenuStrip = this.menuStrip1;
+            this.MaximumSize = new System.Drawing.Size(1100, 1028);
+            this.MinimumSize = new System.Drawing.Size(1100, 1028);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Langton\'s Ant";
+            this.TopMost = true;
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem mbStart;
+        private System.Windows.Forms.ToolStripMenuItem mbStop;
+        private System.Windows.Forms.Label Counter;
+        private System.Windows.Forms.ToolStripMenuItem mbNewGame;
+        private System.Windows.Forms.ToolStripMenuItem mbNext;
+    }
+}
+
---
+++ Langton's ant/Form1.cs
@@ -0,0 +1,219 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Threading;
+
+namespace Langton_s_ant
+{
+    public partial class Form1 : Form
+    {
+        private static int[] AntPosition;
+        private static int length = 5;
+        private static int numberOfCells = 150;
+        private static bool[,] colorStorage;
+        private bool ok = true;
+        private int iterations = 0;
+        private static int[] oldPosition;
+        private char direction = 's';
+        private delegate void EmptyDelegate();
+
+        public Form1()
+        {
+            InitializeComponent();
+
+
+            AntPosition = new int[2];
+            AntPosition[0] = numberOfCells / 2;
+            AntPosition[1] = numberOfCells / 2;
+            oldPosition = new int[2];
+            oldPosition[0] = AntPosition[0];
+            oldPosition[1] = AntPosition[1];
+
+            colorStorage = new bool[numberOfCells, numberOfCells];
+
+            for (int i = 0; i < numberOfCells; i++)
+            {
+                for (int j = 0; j < numberOfCells; j++)
+                {
+                    colorStorage[i, j] = true;
+                }
+            }
+        }
+
+        protected void DoEvents()
+        {
+            Dispatcher.CurrentDispatcher.Invoke(new EmptyDelegate(delegate { }), DispatcherPriority.Background);
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            Pen myPen = new Pen(Color.LightGray);
+            Brush myBrushTrue = new SolidBrush(Color.White);
+            Brush myBrushFalse = new SolidBrush(Color.Black);
+            Brush ant = new SolidBrush(Color.Red);
+
+            for (int i = 0; i < numberOfCells; i++)
+            {
+                for (int j = 0; j < numberOfCells; j++)
+                {
+                    if (i == AntPosition[0] && j == AntPosition[1])
+                    {
+                        //g.DrawRectangle(myPen, 40 + i * length, 40 + j * length, length, length);
+                        //g.FillRectangle(ant, (40 + i * length) + 1, (40 + j * length) + 1, length - 2, length - 2);
+                        g.FillRectangle(ant, (40 + i * length), (40 + j * length), length, length);
+                    }
+                    else if (colorStorage[i, j])
+                    {
+                        //g.DrawRectangle(myPen, 40 + i * length, 40 + j * length, length, length);
+                        //g.FillRectangle(myBrushTrue, (40 + i * length) + 1, (40 + j * length) + 1, length - 2, length - 2);
+                        g.FillRectangle(myBrushTrue, (40 + i * length), (40 + j * length), length, length);
+                    }
+                    else
+                    {
+                        //g.DrawRectangle(myPen, 40 + i * length, 40 + j * length, length, length);
+                        //g.FillRectangle(myBrushFalse, (40 + i * length) + 1, (40 + j * length) + 1, length - 2, length - 2);
+                        g.FillRectangle(myBrushFalse, (40 + i * length), (40 + j * length), length, length);
+                    }
+                }
+            }
+        }
+
+        private void mbStart_Click(object sender, EventArgs e)
+        {
+            if (!ok)
+            {
+                ok = true;
+                mbStart.Text = "Start";
+            }
+
+            while (ok)
+            {
+                Counter.Text = "Steps: " + Convert.ToString(iterations);
+
+                MovingOfTheAnt();
+                //System.Threading.Thread.Sleep(1000);
+
+                Refresh();
+
+                DoEvents();
+
+                iterations++;
+            }
+        }
+
+        private void MovingOfTheAnt()
+        {
+
+            if (AntPosition[0] >= numberOfCells || AntPosition[1] >= numberOfCells)
+            {
+                MessageBox.Show("You've reached the border of the table.");
+                mbStart.Enabled = false;
+                mbStop.Enabled = false;
+                mbNext.Enabled = false;
+                ok = false;
+                return;
+            }
+
+            oldPosition[0] = AntPosition[0];
+            oldPosition[1] = AntPosition[1];
+
+            if (colorStorage[AntPosition[0], AntPosition[1]])
+            {
+                colorStorage[AntPosition[0], AntPosition[1]] = false;
+                
+                switch (direction)
+                {
+                    case 'n':
+                        direction = 'e';
+                        AntPosition[1] += 1;
+                        break;
+                    case 'e':
+                        direction = 's';
+                        AntPosition[0] -= 1;
+                        break;
+                    case 's':
+                        direction = 'w';
+                        AntPosition[1] -= 1;
+                        break;
+                    case 'w':
+                        direction = 'n';
+                        AntPosition[0] += 1;
+                        break;
+                }
+            }
+            else
+            {
+                colorStorage[AntPosition[0], AntPosition[1]] = true;
+
+                switch (direction)
+                {
+                    case 'n':
+                        direction = 'w';
+                        AntPosition[1] -= 1;
+                        break;
+                    case 'w':
+                        direction = 's';
+                        AntPosition[0] -= 1;
+                        break;
+                    case 's':
+                        direction = 'e';
+                        AntPosition[1] += 1;
+                        break;
+                    case 'e':
+                        direction = 'n';
+                        AntPosition[0] += 1;
+                        break;
+                }
+            }
+        }
+
+        private void mbStop_Click(object sender, EventArgs e)
+        {
+            ok = false;
+            mbStart.Text = "Continue";
+        }
+
+        private void mbNewGame_Click(object sender, EventArgs e)
+        {
+            AntPosition[0] = numberOfCells / 2;
+            AntPosition[1] = numberOfCells / 2;
+            oldPosition[0] = AntPosition[0];
+            oldPosition[1] = AntPosition[1];
+            iterations = 0;
+            mbStart.Text = "Start";
+
+            for (int i = 0; i < numberOfCells; i++)
+            {
+                for (int j = 0; j < numberOfCells; j++)
+                {
+                    colorStorage[i, j] = true;
+                }
+            }
+
+            Counter.Text = "Steps: " + Convert.ToString(iterations);
+
+            mbStart.Enabled = true;
+            mbStop.Enabled = true;
+
+            Refresh();
+        }
+
+        private void mbNext_Click(object sender, EventArgs e)
+        {
+                MovingOfTheAnt();
+
+                iterations++;
+                Counter.Text = "Steps: " + Convert.ToString(iterations);
+
+                Refresh();
+        }
+    }
+}
